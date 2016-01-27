de9ac6a3aa65d363b075fb1a68b1797181aa7faa
Apple <apple@Apples-MacBook-Air-8.local>
2016-01-27 18:39:51 +0700

    init

--- view.py
+++ view.py
@@ -1,12 +1,20 @@
+import subprocess
+# from uuid import uuid4
 from flask import render_template, request, Response, jsonify
 from json import dumps
 from wsgi import app
+from pydoop import hdfs
 
 __author__ = 'an'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
+		# d = uuid4()
+		# a = subprocess.Popen(['hadoop', 'fs', '-get', '/caption/DarkKnight.vtt', '/vagrant'], stdout=subprocess.PIPE)
+		# print(a)
+		f = hdfs.open('/caption/DarkKnight.vtt')
+		print(f)
 		return render_template('index.html')
 	else:
 		try:
